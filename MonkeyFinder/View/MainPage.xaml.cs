48d70ac7b614a04b40d3e96102064b3ae288c1b4
Kevin Klose <kevin.klose@teamdynamix.com>
2024-01-17 16:53:56 -0500

    first commit

---
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Telerik.Maui.Controls;
+using Telerik.Maui.Controls.RichTextEditor;
+
+namespace MonkeyFinder.View;
+
+public partial class MainPage : ContentPage
+{
+  public MainPage(MonkeysViewModel viewModel)
+  {
+    InitializeComponent();
+    BindingContext = viewModel;
+  }
+}
+
---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+  public MonkeysViewModel()
+  {
+    Items = new ObservableCollection<string>();
+  }
+
+  [ObservableProperty]
+  private bool _isBusy;
+
+  public ObservableCollection<string> Items { get; set; }
+
+  [RelayCommand]
+  async Task ReloadAsync()
+  {
+    IsBusy = true;
+    await Task.Delay(2000);
+    for (int i = 0; i < 300; i++)
+    {
+      Items.Add(i.ToString());
+    }
+    IsBusy = false;
+  }
+}
